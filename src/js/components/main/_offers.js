498351936f7f61218e0119e31df71b81336d0344
Nechit Elena <lnek10.1990@gmail.com>
2023-02-08 15:39:05 +0200

    made layout of offers section

--- src/js/components/main/_offers.js
+++ src/js/components/main/_offers.js
@@ -1,5 +1,14 @@
 import Swiper from "swiper/bundle";
 
 export function offersSwiperInit() {
-
+  const offersSwiper = new Swiper('.offers__swiper', {
+    grabCursor: true,
+    spaceBetween: 32,
+    slidesPerView: 'auto',
+    slidesPerGroup: 3,
+    navigation: {
+      nextEl: '.swiper-nav__next',
+      prevEl: '.swiper-nav__prev'
+    }
+  })
 }
